2fbe2b66bf03f1968e7be976a5187502e98f5a05
N-Archer <archer.nj@gmail.com>
2021-05-27 01:06:40 +0000

    first commit

---
+++ src/js/component/card.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export const Card = props => {
+	return (
+		<div className="card" style={{ width: "18rem" }}>
+			<img src={props.img} className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">{props.person.name}</h5>
+				<p className="card-text">
+					{props.label1} {props.person.height}
+				</p>
+				<p className="card-text">
+					{props.label2} {props.person.gender}
+				</p>
+				<p className="card-text">
+					{props.label3} {props.person.eye_color}
+				</p>
+				<button type="button" className="btn btn-primary">
+					Details
+				</button>
+				<button type="button" className="btn btn-primary">
+					<i className="far fa-heart" />
+				</button>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	person: PropTypes.object,
+	label1: PropTypes.string,
+	label2: PropTypes.string,
+	label3: PropTypes.string,
+	img: PropTypes.string
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Card } from "../component/card.js";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [characters, setCharacters] = useState([]);
+	const [planets, setPlanets] = useState([]);
+	const [vehicles, setVehicles] = useState([]);
+
+	useEffect(() => {
+		fetch("https://swapi.dev/api/people/")
+			.then(function(response) {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(function(responseAsJson) {
+				setCharacters(responseAsJson.results);
+			})
+			.catch(function(error) {
+				console.log("Looks like there was a problem: \n", error);
+			});
+
+		fetch("https://swapi.dev/api/planets/")
+			.then(function(response) {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(function(responseAsJson) {
+				setPlanets(responseAsJson.results);
+			})
+			.catch(function(error) {
+				console.log("Looks like there was a problem: \n", error);
+			});
+
+		fetch("https://swapi.dev/api/starships/")
+			.then(function(response) {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(function(responseAsJson) {
+				setVehicles(responseAsJson.results);
+			})
+			.catch(function(error) {
+				console.log("Looks like there was a problem: \n", error);
+			});
+	}, []);
+
+	return (
+		<div className="container-fluid">
+			<h1>Characters</h1>
+			<div className="d-flex">
+				{characters.length > 1 &&
+					characters.map((character, index) => {
+						return (
+							<Card
+								img="https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Stormtrooper_%28Star_Wars%29.png/220px-Stormtrooper_%28Star_Wars%29.png"
+								label1="Height:"
+								label2="Gender:"
+								label3="Eye Color:"
+								person={character}
+								key={index}
+							/>
+						);
+					})}
+			</div>
+			<div>
+				<h1>Planets</h1>
+			</div>
+			<div>
+				<h1>Vehicles</h1>
+			</div>
+		</div>
+	);
+};
